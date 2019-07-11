50776cbc7d30e2ebd70566973680a390cae33e7f
crowberios <crowberios@gmail.com>
2019-07-11 17:29:49 +0300

    MSS-1: Configure project

---
+++ src/main/java/com/epam/controllers/HomePageController.java
@@ -0,0 +1,15 @@
+package com.epam.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomePageController {
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView model) {
+        model.setViewName("index");
+        model.addObject("message", "Hello World!");
+        return model;
+    }
+}
